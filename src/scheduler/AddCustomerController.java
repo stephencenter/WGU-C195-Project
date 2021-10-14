f149361f53e12b344f61012e14046514a60bb677
stephencenter <stephenbcenter@gmail.com>
2021-10-14 10:42:58 -0400

    More javadoc comments

--- src/scheduler/AddCustomerController.java
+++ src/scheduler/AddCustomerController.java
@@ -150,11 +150,21 @@ public class AddCustomerController {
             return;
         }
 
+        // Query the database for a list of all divisions. Convert it to a stream, then filter the stream
+        // to only the elements that have a matching CountryID. Finally we convert it to an ObservableList
+        // and use it to populate the division combobox
         Stream<Division> division_stream = Database.GetDivisionList().stream().filter(c -> c.getCountryId() == chosen_country.getId());
         division_combo.setItems(division_stream.collect(Collectors.toCollection(FXCollections::observableArrayList)));
         division_combo.setDisable(false);
     }
 
+    /**
+     * This method is called when hitting the "save" button. It takes all of the information
+     * entered into the forms, verifies that it's valid, and then sends it off to the database
+     * @param event a JavaFX event
+     * @throws SQLException Saving the customer's information to the database could throw an error
+     * @throws IOException Returning to the table form could throw an error
+     */
     public void SaveCustomerInfo(Event event) throws SQLException, IOException {
         String name = name_field.getText();
         String address = address_field.getText();
@@ -162,20 +172,26 @@ public class AddCustomerController {
         String phonenum = phonenum_field.getText();
         Division division = division_combo.getValue();
 
+        // We analyze the information from the forms for errors.
+        // If there are errors then we do not save the information to the database,
+        // and we alert the user of the errors
         List<String> error_list = GetFormErrors(name, address, zipcode, phonenum, division);
         if (!error_list.isEmpty()) {
             error_label.setVisible(true);
             error_label.setText(error_list.get(0));
 
+            // The secondary label says how many additional errors there were
             error_count_label.setVisible(true);
             if (error_list.size() > 2) {
                 error_count_label.setText(String.format("%s more problems found", error_list.size() - 1));
             }
 
+            // If there are only two errors, then the secondary label will instead display the second error
             else if (error_list.size() == 2) {
                 error_count_label.setText(error_list.get(1));
             }
 
+            // If there was only one error then we set the label to be invisible
             else {
                 error_count_label.setVisible(false);
             }
@@ -183,6 +199,7 @@ public class AddCustomerController {
             return;
         }
 
+        // Call the appropriate database method depending on whether we're editing or adding a customer
         if (customer_to_edit == null) {
             Database.AddCustomerToDatabase(name, address, zipcode, phonenum, division);
         }
@@ -190,9 +207,21 @@ public class AddCustomerController {
             Database.UpdateExistingCustomer(name, address, zipcode, phonenum, division, customer_to_edit.getId());
         }
 
+        // Once the customer has been added/updated, we return to the table
         ReturnToCustomerTableForm(event);
     }
 
+    /**
+     * This method analyzes the information the user entered into the form and checks it for errors.
+     * It returns a list of all the errors detected so the user knows what mistakes were made
+     *
+     * @param name The text from the name field
+     * @param address The text from the address field
+     * @param zipcode The text from the zipcode field
+     * @param phonenum The text from the phone number field
+     * @param division The division selected from the division combobox
+     * @return Return a list of all errors detected
+     */
     public List<String> GetFormErrors(String name, String address, String zipcode, String phonenum, Division division) {
         List<String> list_of_errors = new ArrayList<>();
 
@@ -235,6 +264,12 @@ public class AddCustomerController {
         return list_of_errors;
     }
 
+    /**
+     * This method is called when hitting either the "cancel" or "save" buttons.
+     * It switches the current form to the customer table form
+     * @param event a JavaFX event
+     * @throws IOException Switching forms could raise an IOException
+     */
     public void ReturnToCustomerTableForm(Event event) throws IOException {
         Main.LoadForm(getClass().getResource("CustomerTableForm.fxml"), event, "Customer Table");
     }
